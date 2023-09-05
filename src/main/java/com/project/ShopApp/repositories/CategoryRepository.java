194a4f223ad98f62548b7e10d03c2ea1edae61b5
caosang92 <caosang92@gmail.com>
2023-09-05 18:15:37 +0900

    login

--- src/main/java/com/project/ShopApp/repositories/CategoryRepository.java
+++ src/main/java/com/project/ShopApp/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.project.ShopApp.Models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
